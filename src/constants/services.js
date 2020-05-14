99585522ef91557d0138806ec27d63670778e1c0
FranzLiebkind <milomir.djuric@limit.rs>
2020-05-14 23:35:05 +0200

    first commit

---
+++ src/constants/services.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sapiente officia dolorum.",
+  },
+  {
+    icon: <FaTree />,
+    title: "endles hiking",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sapiente officia dolorum.",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sapiente officia dolorum.",
+  },
+]
